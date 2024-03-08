303e60f9b32fbf8eba617b88d0d975b31a0c9211
Melody Oluchi <melodyoluchi848@gmail.com>
2024-03-08 22:42:05 +0100

    first commit

---
+++ src/main/java/africa/semicolon/EcommerceApplication/ECommerceApplication.java
@@ -0,0 +1,12 @@
+package africa.semicolon.EcommerceApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
